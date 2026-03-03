519d76648739d783df3490c4a3cc971ef5b52e83
Anatolii Rabochauskas <tolyachaus@Mac.fritz.box>
2026-03-03 16:05:12 +0100

    fix: correct import path for local fonts CSS to fix build failure

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './assets/fonts/montserrat.css';
+import './src/assets/fonts/montserrat.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
