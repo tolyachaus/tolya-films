b03154e6489dc564b968a8be9b51d866852f2818
Anatolii Rabochauskas <tolyachaus@Mac.fritz.box>
2026-03-03 22:13:26 +0100

    feat(assets): swap external showreel for local 58MB video file

--- types.ts
+++ types.ts
@@ -19,7 +19,7 @@ export const ASSETS = {
   logoWhite: 'https://tolyafilms.com/wp-content/uploads/2025/06/logo_tolya_films_white.png',
   logoBlack: 'https://tolyafilms.com/wp-content/uploads/2025/06/logo_tolya_films_black.png',
   profilePhoto: 'https://tolyafilms.com/wp-content/uploads/2025/10/DSC01515-3-scaled.jpg',
-  showreel: 'https://tolyafilms.com/wp-content/uploads/2025/11/schowreel_2025_HDV1.mp4',
+  showreel: '/schowreel_2025_HDV1.mp4',
 };
 
 export const SOCIAL_LINKS = {
